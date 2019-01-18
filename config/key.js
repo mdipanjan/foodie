7019c820c89e683006635d843c3e9f9bf4ace82e
mdipanjan <mdipanjan33@gmail.com>
2019-01-18 05:29:34 -1200

    'change'

--- config/key.js
+++ config/key.js
@@ -1,6 +1,8 @@
 module.exports = {
+    
     googleClientID:'553924212565-7guf06fpe99nlm20p64u92h8cqtdahj2.apps.googleusercontent.com',
     googleClientSecret:'C94sp0cPwRXUw1g6nPVWGGrd',
     monoURI:'mongodb://mdipanjan:mdipanjan1@ds261114.mlab.com:61114/foodie-dev',
-    cookieKey:'dggddhsjdldlfjfhfbfebffebfkhb3kr3vjfjojobr3yr'
+    cookieKey:'dggddhsjdldlfjfhfbfebffebfkhb3kr3vjfjojobr3yr',
+    redirectURI:'https://sheltered-plains-97034.herokuapp.com'
 };
