5a4ed0c8eba7af8eb9eac6e849b76872d4504e60
mdipanjan <mdipanjan33@gmail.com>
2019-01-18 15:19:13 -1200

    'auth'

--- config/key.js
+++ config/key.js
@@ -1,8 +1,5 @@
-module.exports = {
-    
-    googleClientID:'553924212565-7guf06fpe99nlm20p64u92h8cqtdahj2.apps.googleusercontent.com',
-    googleClientSecret:'C94sp0cPwRXUw1g6nPVWGGrd',
-    monoURI:'mongodb://mdipanjan:mdipanjan1@ds261114.mlab.com:61114/foodie-dev',
-    cookieKey:'dggddhsjdldlfjfhfbfebffebfkhb3kr3vjfjojobr3yr'
-   
-};
+if(process.env.NODE_ENV === 'production'){
+    module.exports = require('./prod');
+}else{
+    module.exports = require('./dev');
+}
